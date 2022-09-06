3a9b6ef572f143c872863991b473422e7b3bb58c
user1 <user1@172.16.3.226>
2022-09-06 17:05:52 +0900

    aaa

--- test/src/test/Gittest.java
+++ test/src/test/Gittest.java
@@ -14,7 +14,7 @@ public class Gittest {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!");
+		System.out.println("Hello World2!");
 	}
 
 }
