fc8a5dabefb8daac79890eb7545edbfe53318020
user1 <user1@172.16.3.226>
2022-09-06 15:39:54 +0900

    はじめてのコミット

---
+++ test/src/test/Gittest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author user1
+ *
+ */
+public class Gittest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+	}
+
+}
