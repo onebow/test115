09cee9b1470c1dc62487f81b3508ec0e59e4f72f
user1 <user1@172.16.3.226>
2022-09-06 15:44:12 +0900

    2kaime

---
+++ test/src/test/Test2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author user1
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+	}
+
+}
